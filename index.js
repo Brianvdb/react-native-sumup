ded8375e88a09b6abfedaf2db56fd4f4b67e4b80
Brian <brianvdb11@gmail.com>
2019-02-12 20:28:26 +0100

    charge

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ class RNSumup {
     static login() {
         NativeRNSumup.login();
     }
+
+    static charge(amount) {
+        NativeRNSumup.charge(amount);
+    }
 }
 
 export default RNSumup;
